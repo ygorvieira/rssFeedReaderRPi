43a9f07b4e13c9cc3daa85db3a9635bb884ee8f0
Ygor Vieira <ygorus@gmail.com>
2026-05-21 23:50:26 -0300

    update feeds

--- feeds.py
+++ feeds.py
@@ -1,22 +1,53 @@
 FEEDS = {
 	"Dev": {
-		"Hacker News": "https://news.ycombinator.com/rss",
 		".NET": "https://devblogs.microsoft.com/dotnet/feed/",
-		"It's FOSS": "https://itsfoss.com/feed/"
+		"Ars Technica": "https://feeds.arstechnica.com/arstechnica/index",
+		"Código Fonte TV": "https://codigofontetv.com.br/feed/",
+		"Diolinux": "https://diolinux.com.br/feed/",
+		"Github Blog": "https://github.blog/feed/",
+		"Hacker News": "https://news.ycombinator.com/rss",
+		"It's FOSS": "https://itsfoss.com/feed/",
+		"Linus Tech Tips": "https://linustechtips.com/main/news.xml/",
+		"Linux Descomplicado": "https://www.linuxdescomplicado.com.br/feed",
+		"LWN.net": "https://lwn.net/headlines/rss",
+		"OMG Ubuntu": "https://www.omgubuntu.co.uk/feed",
+		"Phoronix": "http://www.phoronix.com/rss.php",
+		"Tableless": "https://tableless.com.br/feed/",
+		"Tech Crunch": "https://techcrunch.com/feed/",
+		"Tecnoblog": "https://tecnoblog.net/limitado/feed/",
+		"Verge Tech": "https://www.theverge.com/rss/index.xml",
 	},
 
 	"Games": {
+		"Destructoid": "https://www.destructoid.com/feed/",
+		"Gamespot": "https://www.gamespot.com/feeds/mashup/",
+		"Game Vício": "https://www.gamevicio.com/rss/",
+		"IGN": "https://feeds.ign.com/ign/games-all",
 		"Kotaku": "https://kotaku.com/rss",
-		"Rock Paper Shotgun": "https://www.rockpapershotgun.com/feed"
+		"Nintendo Blast": "https://www.nintendoblast.com.br/feed/posts/default",
+		"PC Gamer": "https://www.pcgamer.com/rss/",
+		"Polygon": "https://www.polygon.com/rss/index.xml",
+		"PSX Brasil": "https://psxbrasil.com.br/feed/",
+		"Rock Paper Shotgun": "https://www.rockpapershotgun.com/feed",
+		
 	},
 
 	"Anime": {
 		"Anime News Network": "https://www.animenewsnetwork.com/all/rss.xml",
-		"Crunchyroll News": "https://www.crunchyroll.com/newsrss"
+		"Anime United": "https://www.animeunited.com.br/feed/",
+		"Crunchyroll News": "https://www.crunchyroll.com/newsrss",
+		"Intoxianime": "https://www.intoxianime.com/feed",
+		"JBOX": "https://www.jbox.com.br/feed/",
+		"My Anime List News": "https://myanimelist.net/rss/news.xml",
+		"Otaku USA Magazine": "https://otakuusamagazine.com/feed/"
 	},
 
 	"RPG": {
+		"Bell of Lost Souls": "https://www.belloflostsouls.net/feed",
 		"Dicebreaker": "https://www.dicebreaker.com/feed",
-		"EN World": "https://www.enworld.org/forums/news.2/index.rss"
+		"Dungeonist": "https://dungeonist.com/feed/",
+		"EN World": "https://www.enworld.org/forums/news.2/index.rss",
+		"Paizo Blog": "https://paizo.com/community/blog/rss",
+		"Retropunk": "https://retropunk.com.br/feed/",
 	}
 }
